55d1cdf3e142bde26c262b9001f2d5a9dfeb1274
Julius Kvedaras <juliuskvedaras@yahoo.ie>
2016-04-10 19:51:50 +0000

    added styles and ingredients show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:edit, :update, :show, :like]
-  before_action :require_user, except: [:show, :index]
+  before_action :require_user, except: [:show, :index, :like]
+  before_action :require_user_like, only: [:like]
   before_action :require_same_user, only: [:edit, :update]
   
   def index
@@ -67,5 +68,12 @@ class RecipesController < ApplicationController
         flash[:danger] = "You can only edit your own requests"
         redirect_to recipes_path
       end
+    end
+    
+    def require_user_like
+      if !logged_in?
+        flash[:danger] = "You must be logged in to perform this action"
+        redirect_to :back
+      end  
     end
 end
